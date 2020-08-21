9181d6c3e77bd32a94916bfa52d5e94ec7911125
ishenbody <1733750512@qq.com>
2020-08-21 11:03:05 +0800

    提交一次

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,12 @@
     <img src="./assets/logo.png">
     <div>
       <HelloWorld/>
-      <el-button>el-button</el-button>
+    <el-button>el-button</el-button>
     </div>
-   
   </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
@@ -19,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
